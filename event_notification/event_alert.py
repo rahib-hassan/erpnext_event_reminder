022e0bc81619e333a4462c78d88e02bfb881dd60
rahib-hassan <rahib.hassan10@gmail.com>
2022-02-22 07:35:06 +0530

    feat: send notification to participants

--- event_notification/event_alert.py
+++ event_notification/event_alert.py
@@ -3,13 +3,27 @@ from frappe.utils import now
 from datetime import datetime, timedelta
 from frappe import enqueue
 
-def notification_log(event):
-    notification = frappe.get_doc({
-        "doctype": "Notification Log", "subject": event.subject, "for_user": event.owner,\
-        "type": "Alert", "email_content": event.description, "document_type": "Event",\
-        "document_name": event.name})
+def get_participants(doc):
+    users = []
+    participants = doc.event_participants
+    for participant in participants:
+        if participant.reference_doctype == "Employee":
+            user_id = frappe.db.get_value('Employee', {'full_name': participant.reference_docname}, ['user_id'])
+            users.append(user_id)
+    return users
 
-    notification.insert()
+def notification_log(doc):
+    subject = (f'"{doc.subject}" starts in {doc.alert_time} minutes')
+    
+    users = get_participants(doc)
+    users.append(doc.owner)
+    for user in users:
+        notification = frappe.get_doc({
+            "doctype": "Notification Log", "subject": subject, "for_user": doc.owner,\
+            "type": "Alert", "email_content": doc.description, "document_type": "Event",\
+            "document_name": doc.name})
+
+        notification.insert()
 
 
 def send_notification():
@@ -21,7 +35,8 @@ def send_notification():
             alert_time = event.starts_on - timedelta(minutes=event.alert_time)
             cur_time = datetime.strptime(now(), '%Y-%m-%d %H:%M:%S.%f')
             if cur_time >= alert_time and cur_time <= event.starts_on:
-                enqueue(notification_log, now=False, queue="default")
-                notification_log(event)
+                enqueue(notification_log, now=False, queue="default", doc=event)
                 event.alert_sent = 1
                 event.save()
+
+
